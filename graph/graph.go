ca9c3c372e75a97b68b14713deb6206d711cb438
Jonathan Chiam <jczjchiam@gmail.com>
2017-11-22 16:18:31 +0800

    wip: improve graph package

--- graph/graph.go
+++ graph/graph.go
@@ -6,3 +6,25 @@ type Node struct {
 	Parents  map[string]*Node
 	Children map[string]*Node
 }
+
+// Graph is the graph that manages the nodes and which graph operations work on
+type Graph struct {
+	nodes map[string]*Node
+}
+
+// NewGraph initialises a new graph
+func NewGraph() *Graph {
+	return &Graph{
+		nodes: make(map[string]*Node),
+	}
+}
+
+// GetNodes returns the map of nodes in the graph
+func (g Graph) GetNodes() map[string]*Node {
+	return g.nodes
+}
+
+// GetNode returns a single node in the graph given the ID
+func (g Graph) GetNode(id string) *Node {
+	return g.nodes[id]
+}
